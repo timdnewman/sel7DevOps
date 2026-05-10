eb834d662e844ec357f81c64880d16e369aab18f
timdnewman <43032684+timdnewman@users.noreply.github.com>
2026-05-10 17:21:18 +0000

    userform added

--- devOpsAssignment/users/views.py
+++ devOpsAssignment/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+
+from .forms import LoginForm
 
 # Create your views here.
+class UserLoginView(LoginView):
+
+    template_name = "login.html"
+    authentication_form = LoginForm
+    redirect_authenticated_user = True
+
+
+class UserLogoutView(LogoutView):
+
+    next_page = reverse_lazy("users:login")
