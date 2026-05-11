5da2362d3e9c6f48122942df53c573d9826b38de
timdnewman <43032684+timdnewman@users.noreply.github.com>
2026-05-11 13:45:45 +0000

    Updating ruff toml

--- devOpsAssignment/ticketSystem/wsgi.py
+++ devOpsAssignment/ticketSystem/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticketSystem.settings")
+if "WEBSITE_HOSTNAME" in os.environ:
+    settings_module = "ticketSystem.deployment"
+else:
+    settings_module = "ticketSystem.settings"
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_module")
 
 application = get_wsgi_application()
