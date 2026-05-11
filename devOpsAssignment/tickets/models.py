7bf4094e63a1fe360ef18c877315ab9825d9b365
timdnewman <43032684+timdnewman@users.noreply.github.com>
2026-05-11 10:10:29 +0000

    pch test minus double quote bug

--- devOpsAssignment/tickets/models.py
+++ devOpsAssignment/tickets/models.py
@@ -2,18 +2,16 @@ from django.conf import settings
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
-import json
 
 
 class Ticket(models.Model):
-
     class Area(models.TextChoices):
         ENGINEERING = "engineering", "Engineering"
         TECHNOLOGY = "technology", "Technology"
         OPS = "ops", "Ops"
         STRATEGY = "strategy", "Strategy"
-        SUPPORT = "support", "Support" 
-    
+        SUPPORT = "support", "Support"
+
     class Severity(models.TextChoices):
         ABLE = "able", "Able to work"
         PARTIAL = "partial", "Partially able to work"
@@ -24,17 +22,17 @@ class Ticket(models.Model):
         ASSIGNED = "assigned", "Assigned"
         WORKING = "working", "Working"
         CLOSED = "closed", "Closed"
-    
+
     name = models.CharField(
         max_length=100,
         validators=[MinLengthValidator(2)],
         help_text="Name of the person raising the ticket.",
-        )
+    )
     area = models.CharField(max_length=20, choices=Area.choices)
     severity = models.CharField(max_length=20, choices=Severity.choices)
     description = models.TextField(
-            validators=[MinLengthValidator(10)],
-            help_text="Describe the problem in at least 10 characters.",
+        validators=[MinLengthValidator(10)],
+        help_text="Describe the problem in at least 10 characters.",
     )
 
     status = models.CharField(
