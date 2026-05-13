ead7f12e26352049f875b736c4ea1c56dfa8d958
timdnewman <43032684+timdnewman@users.noreply.github.com>
2026-05-13 07:30:10 +0000

    corrected imports

--- devOpsAssignment/ticketSystem/urls.py
+++ devOpsAssignment/ticketSystem/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
-from views import health
+from ticketSystem.views import health
 
 urlpatterns = [
     path("admin/", admin.site.urls),
