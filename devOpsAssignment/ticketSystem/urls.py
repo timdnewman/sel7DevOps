a63c072372182d4a726f9eca0ebe4e1a71aa404c
timdnewman <43032684+timdnewman@users.noreply.github.com>
2026-05-13 07:35:28 +0000

    Ruff checks

--- devOpsAssignment/ticketSystem/urls.py
+++ devOpsAssignment/ticketSystem/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+
 from ticketSystem.views import health
 
 urlpatterns = [
